d7968fd26285daf7451d6aec308836ba7e7bd1b0
user <adiletu40@gmail.com>
2022-06-01 01:41:29 +0600

    создал AppDataBaseModule

---
+++ app/src/main/java/com/example/roomts/di/AppDataBaseModule.kt
@@ -0,0 +1,26 @@
+package com.example.roomts.di
+
+import android.content.Context
+import com.example.data.local.db.AppDataBase
+import com.example.data.local.db.RoomClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppDataBaseModule {
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context) =
+        RoomClient().provideCreateAppDataBase(context)
+
+    @Singleton
+    @Provides
+    fun provideUserDao(appDataBase: AppDataBase) =
+        RoomClient().provideUserDao(appDataBase)
+
+}
