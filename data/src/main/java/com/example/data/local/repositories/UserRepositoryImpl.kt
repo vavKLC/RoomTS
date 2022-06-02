9a17d7d402d666923446caa53ae02c8e382faccb
Clar1fy <timplifier@gmail.com>
2022-06-02 19:27:16 +0600

    Появились функции на костылях

--- data/src/main/java/com/example/data/local/repositories/UserRepositoryImpl.kt
+++ data/src/main/java/com/example/data/local/repositories/UserRepositoryImpl.kt
@@ -1,19 +1,23 @@
 package com.example.data.local.repositories
 
 import com.example.data.local.db.daos.UserDao
+import com.example.data.model.toData
+import com.example.data.model.toDomain
 import com.example.domain.models.UserModel
 import com.example.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val userDao: UserDao
 ) : UserRepository {
     override fun getUsers(): Flow<List<UserModel>> =
-        TODO("функций нету")
+        userDao.getUsers().map { it -> it.map { it.toDomain() } }
 
-    override suspend fun insertAllUser(userModel: UserModel) =
-        TODO("функций нету")
+    override suspend fun insertAllUser(userModel: UserModel) {
+        userDao.insertUser(userModel.toData())
+    }
 
 
 }
